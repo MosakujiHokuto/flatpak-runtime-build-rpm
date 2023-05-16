11e2bf9a194dad81b1b32d56672a1ad95540ca9c
Youkou Tenhouin <youkou@tenhou.in>
2023-05-16 14:46:03 +0800

    Load buildinfo

--- flatpak_postbuild.sh
+++ flatpak_postbuild.sh
@@ -7,4 +7,13 @@ msg() {
 
 msg "Flatpak post build hook invoked"
 
-ls -R /usr/src/packages/SOURCES
+source /usr/src/packages/SOURCES/buildinfo
+
+export FLATPAK_NAME=$name
+export FLATPAK_ARCH=$arch
+export FLATPAK_VERSION=$version
+
+msg "Build Info:"
+msg "\tName:\t$FLATPAK_NAME"
+msg "\tArch:\t$arch"
+msg "\tVersion:\t$version"
